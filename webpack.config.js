54116836a3799e475633a9710bf5591cf754b23c
Peter Schussheim <peter@schussheim.com>
2018-03-30 12:49:34 -0400

    publish

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	mode: 'production',
+	entry: './index.js',
+	output: {
+		filename: 'monaco.min.js',
+		path: path.resolve(__dirname, 'dist'),
+		libraryTarget: 'umd',
+		library: 'monaco',
+	},
+	resolveLoader: {
+		alias: {
+			'blob-url-loader': require.resolve('./loaders/blobUrl'),
+			'compile-loader': require.resolve('./loaders/compile'),
+		},
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [ 'style-loader', 'css-loader' ]
+		}]
+	},
+	plugins: [
+		new webpack.IgnorePlugin(/^((fs)|(path)|(os)|(crypto)|(source-map-support))$/, /vs\/language\/typescript\/lib/),
+		new webpack.optimize.LimitChunkCountPlugin({
+			maxChunks: 1,
+		}),
+	],
+};
